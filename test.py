400a1fd65762a63c91cf9174c66269875b74c7a5
ingo198123 <ingo198123@gmail.com>
2025-01-21 15:41:18 +0100

    Update visual_features_try

--- test.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-import keras
-import numpy as np
-
-print(f"TensorFlow version: {tf.__version__}")
-print(f"Keras version: {keras.__version__}")
-print(f"NumPy version: {np.__version__}")
